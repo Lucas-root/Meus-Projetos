a1df0ad42ca43641712730bcd75fb1b4c8305822
lucas <lucasedux0@gmail.com>
2020-06-22 22:41:44 -0300

    Não finalizei estes programas estão sem pé nem cabeça (não usar)

---
+++ projeto_jogo_velha/armacao.py
@@ -0,0 +1,11 @@
+# Este é a criação de um jogo da velha
+
+print('   |   |   ')
+print(' {} | {} | {} '.format())
+print('___|___|___')
+print('   |   |   ')
+print(' {} | {} | {} '.format())
+print('___|___|___')
+print('   |   |   ')
+print(' {} | {} | {} '.format())
+print('   |   |   ')
