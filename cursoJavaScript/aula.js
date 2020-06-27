153bb6320fb11f7b579d72b4e9909bfccecc10de
lucas <lucasedux0@gmail.com>
2020-06-27 17:58:00 -0300

    Removi algumas files que estavam sedo usadas para teste e modifiquei alguns arquivos mas nada de importante

--- cursoJavaScript/aula.js
+++ cursoJavaScript/aula.js
@@ -1,5 +1,17 @@
-console.log('Lucas')
-console.log('Bem vindo ao curso')
-console.log('Hello')
+let nome = 'Lucas';
+let idade = 25;
+let estaAprovado = true;
+let sobrenome = undefined;
+let corSelecionada = null;
+console.log(nome);
+console.log(idade);
+console.log(estaAprovado);
+console.log(sobrenome);
+console.log(corSelecionada);
+console.log(1+2);
+console.log('Criei o programa v2 agora com a opção de escolha do diretorio')
+console.log('Mudei')
+console.log('Alterei')
+
 
 
