63fbe5e69b4f5527caa43024e1a7a4b62469310e
lucas <lucasedux0@gmail.com>
2020-06-27 18:01:31 -0300

    Este commit é apenas para a file aula.js e nela não tem nada de importante

--- cursoJavaScript/aula.js
+++ cursoJavaScript/aula.js
@@ -9,9 +9,6 @@ console.log(estaAprovado);
 console.log(sobrenome);
 console.log(corSelecionada);
 console.log(1+2);
-console.log('Criei o programa v2 agora com a opção de escolha do diretorio')
-console.log('Mudei')
-console.log('Alterei')
-
+console.log)
 
 
